151dd9b20f9d2e7a6b06b8d5f3a93c25125db360
Yasmin <yasmincavalheirodasilva@gmail.com>
2026-05-05 21:44:34 -0300

    feat: adicionar controle de disponibilidade

--- Livro.java
+++ Livro.java
@@ -2,11 +2,13 @@ public class Livro {
     String nome;
     String autor;
     int paginas;
+    boolean disponivel;
 
     public Livro(String nome, String autor, int paginas) {
         this.nome = nome;
         this.autor = autor;
         this.paginas = paginas;
+        this.disponivel = true;
  
    }
    public void MostrarInformacoes() {
